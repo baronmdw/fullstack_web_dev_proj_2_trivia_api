2f96b982a7d5498f1ccb7477106b43ca32ce09f8
baronmdw <58744855+baronmdw@users.noreply.github.com>
2023-03-07 21:21:55 +0100

    setting up testing

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -12,10 +12,11 @@ class TriviaTestCase(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
+        # TODO: make sure to initiate the correct database, see: https://stackoverflow.com/questions/75523569/runtimeerror-a-sqlalchemy-instance-has-already-been-registered-on-this-flask
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}:{}{}/{}".format('student','student','localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -29,6 +30,10 @@ class TriviaTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
+    def test_Hello(self):
+        res = self.client().get("/")
+
+        self.assertEqual(res, "Hello World")
     """
     TODO
     Write at least one test for each test for successful operation and for expected errors.
